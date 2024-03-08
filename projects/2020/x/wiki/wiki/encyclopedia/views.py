1df1c1a0db82b19f6ea84064ebb3abfb158842fb
Rafał Tomczyk <rafal.tomczyk2000@gmail.com>
2024-03-08 17:16:59 +0100

    trying to work on 3 assigment

--- projects/2020/x/wiki/wiki/encyclopedia/views.py
+++ projects/2020/x/wiki/wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
+from django.http import Http404
 from django.shortcuts import render
+from .util import get_entry
 
 from . import util
 
@@ -8,3 +10,11 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry_name(request, title):
+    content = get_entry(title)
+    if content is not None:
+         return render(request, "encyclopedia/entry.html", {"title": title, "content": content})
+    else:
+        raise Http404("Entry does not exist")
+     
